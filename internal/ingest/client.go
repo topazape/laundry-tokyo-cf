44a1dc8925eda51b2d56531cf9431bdfd93464d4
topazape <topazape@gmail.com>
2026-08-22 10:11:29 +0900

    chore: Go 1.27 へ更新して encoding/json/v2 へ移行し、wrangler のバージョンも更新

--- internal/ingest/client.go
+++ internal/ingest/client.go
@@ -3,7 +3,7 @@ package ingest
 import (
 	"bytes"
 	"context"
-	"encoding/json"
+	"encoding/json/v2"
 	"fmt"
 	"io"
 	"net/http"
--- internal/laundrich/shops.go
+++ internal/laundrich/shops.go
@@ -2,7 +2,7 @@ package laundrich
 
 import (
 	"bytes"
-	"encoding/json"
+	"encoding/json/v2"
 	"fmt"
 	"slices"
 	"strconv"
